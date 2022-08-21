b810f48c59485c6f15077bc1ff600f106596aec4
MutheuLinet <muneenilinet@gmail.com>
2022-08-21 13:50:35 +0300

    Project resources and test files

---
+++ src/test/java/com/lyn/ra/RoomAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyn.ra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
