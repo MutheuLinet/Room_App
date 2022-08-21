1887adfb4989b98550531c131ac71acb6688bb1f
MutheuLinet <muneenilinet@gmail.com>
2022-08-21 13:47:36 +0300

    Rom application entry point

---
+++ src/main/java/com/lyn/ra/RoomAppApplication.java
@@ -0,0 +1,13 @@
+package com.lyn.ra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomAppApplication.class, args);
+	}
+
+}
